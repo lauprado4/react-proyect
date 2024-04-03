315405fb71b99648abff5e129e39a684722d1dd3
Lautaro Prado <lauprado04@gmail.com>
2024-04-02 23:05:21 -0300

    Corrigiendo errores

--- appreact/src/componentes/Item.jsx
+++ appreact/src/componentes/Item.jsx
@@ -6,17 +6,14 @@ import "./Item.css"
 export const Item = ({item}) => {
 
     return(
-        <Card className="item-card" style={{ width: '18rem'}}>
-      <Card.Img variant="top" src={item.img} />
-      <Card.Body>
-        <Card.Title>{item.name}</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Card.Text>${item.price}</Card.Text>
-        <Link to={`/item/${item.id}`}><Button variant="primary">Ver mas</Button></Link>
-      </Card.Body>
-    </Card>
+      <Card className="item-card" style={{ width: '18rem'}}>
+        <Card.Img variant="top" src={item.img} />
+        <Card.Body>
+          <Card.Title className='card-title'>{item.name}</Card.Title>
+          <Card.Text className='card-description'>{item.description}</Card.Text>
+          <Card.Text>${item.price}</Card.Text>
+          <Link to={`/item/${item.id}`}><Button variant="primary">Ver mas</Button></Link>
+        </Card.Body>
+      </Card>
   );
  }
